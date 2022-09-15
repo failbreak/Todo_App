c8dda7f46702d66beee57fd1778e797a03483002
Max Møenbo Larsen <maxx0696@elevcampus.dk>
2022-09-15 13:07:48 +0200

    Added methods to interface

--- Todo_App/Repository/ITaskRepo.cs
+++ Todo_App/Repository/ITaskRepo.cs
@@ -1,7 +1,12 @@
-﻿namespace Todo_App.Repository
+﻿using Todo_App.Model;
+
+namespace Todo_App.Repository
 {
     public interface ITaskRepo
     {
-
+        void AddTask(string Title, string Desc);
+        void DelTask(string guid);
+        void EditTask(string guid, string desc, string title, int prio, bool isComp);
+        To_Do? GetTask(string guid);
     }
 }
