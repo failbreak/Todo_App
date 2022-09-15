0924eb2b34727387f6f706aef98e99e0b6daace0
Max Møenbo Larsen <maxx0696@elevcampus.dk>
2022-09-15 12:29:32 +0200

    Added To-Do.cs

---
+++ Todo_App/Model/To-Do.cs
@@ -0,0 +1,22 @@
+﻿namespace Todo_App.Model
+{
+    public class To_Do
+    {
+        public string Guid { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime Created { get; set; }
+        public enum Priority { Low, Normal, High }
+        public Priority TaskPriority { get; set; }
+        public bool IsCompleted { get; set; }
+        public To_Do(string tasktitle, string description, string guid )
+        {
+            TaskTitle = tasktitle;
+            TaskDescription = description;
+            Created = DateTime.Now;
+            Guid = guid;
+            TaskPriority = Priority.Normal;
+            IsCompleted = false;
+        }
+    }
+}
