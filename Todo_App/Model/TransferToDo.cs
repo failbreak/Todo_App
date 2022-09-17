6e549e188576d2851eb24775697a345e32a1251b
Empire Works <Empire Works@DESKTOP-U7R6FSG>
2022-09-17 23:31:58 +0200

    added little to the html

---
+++ Todo_App/Model/TransferToDo.cs
@@ -0,0 +1,13 @@
+﻿namespace Todo_App.Model
+{
+    public class TransferToDo
+    {
+        //List<To_Do>
+        List<TransferToDo> transferToDoList;
+        public TransferToDo()
+        {
+            //transferToDoList =
+        }
+
+    }
+}
