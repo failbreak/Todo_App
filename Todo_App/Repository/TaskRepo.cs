56fab129bbff4609ee675078c6234fa7ef8acfe3
Max Møenbo Larsen <maxx0696@elevcampus.dk>
2022-09-21 08:55:04 +0200

    Documentation now there

--- Todo_App/Repository/TaskRepo.cs
+++ Todo_App/Repository/TaskRepo.cs
@@ -10,10 +10,35 @@ namespace Todo_App.Repository
         {
             _toDoList = new List<To_Do>();
         }
+
+        /// <summary>
+        ///  Adds a task to the list
+        /// </summary>
+        /// <param name="priot"></param>
+        /// <param name="Title"></param>
+        /// <param name="Desc"></param>
         public void AddTask(To_Do.Priority priot, string Title, string Desc) => _toDoList.Add(new(priot, Title, Desc));
 
+        /// <summary>
+        /// Finds a specific thing from list using guid
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
         public To_Do? GetTask(string guid) => _toDoList.Find(x => x.GUID.ToString() == guid);
+
+        /// <summary>
+        /// Gets all tasks
+        /// </summary>
+        /// <returns></returns>
         public List<To_Do> GetallTask() => _toDoList;
+        /// <summary>
+        /// edits a specific task
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <param name="desc"></param>
+        /// <param name="title"></param>
+        /// <param name="prio"></param>
+        /// <param name="isComp"></param>
         public void EditTask(string guid, string desc, string title, int prio, bool isComp)
         {
             To_Do? todo = GetTask(guid);
@@ -22,11 +47,20 @@ namespace Todo_App.Repository
             todo.TaskPriority = (To_Do.Priority)prio;
             todo.IsCompleted = isComp;
         }
+
+        /// <summary>
+        /// Deletes a specific task using guid
+        /// </summary>
+        /// <param name="guid"></param>
         public void DelTask(string guid)
         {
             _toDoList.Remove(GetTask(guid));
         }
             
+        /// <summary>
+        /// Sets a specific Task status as complete
+        /// </summary>
+        /// <param name="guid"></param>
         public void CompTask(string guid) => GetTask(guid).IsCompleted = true;
     }
 }
